392a502969cbdf66b229db5c4b6a6c4650358c50
Sergey Zubkov <svzubkovru@gmail.com>
2022-11-18 14:44:57 +0300

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,7 @@
 
 // Scripts for firebase and firebase messaging
-importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
-importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
-
+ importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
+ importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
   apiKey: "AIzaSyAp8ueQBS1IwK-IAlewoUCAwETsJLiU7fk",
